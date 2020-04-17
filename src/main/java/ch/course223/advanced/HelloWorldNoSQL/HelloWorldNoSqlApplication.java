b839a09c1283929bd5ded2bf2a6f2c9fb3edd506
Ivo Gurtner <ivo.gurtner@edu.tbz.ch>
2020-04-17 08:59:01 +0200

    Add project

---
+++ src/main/java/ch/course223/advanced/HelloWorldNoSQL/HelloWorldNoSqlApplication.java
@@ -0,0 +1,13 @@
+package ch.course223.advanced.HelloWorldNoSQL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldNoSqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldNoSqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/course223/advanced/HelloWorldNoSQL/HelloWorldNoSqlApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.course223.advanced.HelloWorldNoSQL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldNoSqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
